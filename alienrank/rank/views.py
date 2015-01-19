2940473ce723d3a88d4a49a9c3dde54c3a6671fb
Ole Filippov <philya@gmail.com>
2015-01-20 01:27:24 +0200

    Add total scores to the table

--- alienrank/rank/views.py
+++ alienrank/rank/views.py
@@ -4,5 +4,5 @@ from rank.models import Domain
 
 def home(request):
     
-    domains = Domain.objects.all().order_by('-post_count', '-score_average')
+    domains = Domain.objects.all().order_by('-score_total', '-post_count', '-score_average')
     return render(request, 'base.html', {'domains': domains})
