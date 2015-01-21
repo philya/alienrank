c59c3981d853d94b93bd5376e7ecada1781f5940
Ole Filippov <philya@gmail.com>
2015-01-21 21:55:27 +0200

    Add last snapshot timestamp

--- alienrank/rank/views.py
+++ alienrank/rank/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 
-from rank.models import Domain, MediaProperty
+from rank.models import Domain, MediaProperty, Snapshot
 
 def home(request):
     
     domains = Domain.objects.all().order_by('-score_total', '-post_count', '-score_average')
     mps = MediaProperty.objects.filter(top_post__isnull=False).filter(top_post__top_place__isnull=False).order_by('top_post__top_place', '-score_total')
 
-    return render(request, 'base.html', {'domains': domains, 'mps': mps})
+    last_snapshot = Snapshot.objects.order_by('-added')[0]
+
+    return render(request, 'base.html', {
+        'domains': domains,
+        'mps': mps,
+        'last_snapshot': last_snapshot,
+    })
